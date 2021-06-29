88332c91f8f3374a784d5872abdff21e46f1a090
409570121 <ch11522914@gmail.com>
2021-06-29 14:24:27 +0800

    2021/06/29/14/24

--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -18,7 +18,7 @@ public class Tester {
         Calendar rightNow = Calendar.getInstance();
         int Hours = rightNow.get(Calendar.HOUR_OF_DAY);
         while (true) {
-            System.out.println("please ch your moive and type moivenumber");
+            System.out.println("please choice your moive and type moivenumber");
             System.out.println("1.Aladdin");
             System.out.println("2.Dumbo");
             System.out.println("3.Mermaid");
@@ -26,23 +26,25 @@ public class Tester {
             int choice = scanner.nextInt();
             if (Hours <= 12 || Hours == 0) {
                 {
-                    System.out.println("現在時間是" + Hours + "所以是早鳥優惠票價為120元");
                     if (choice == 1) {
-                        System.out.println("please type your count");
+                        System.out.println("現在時間是" + Hours + "所以是早鳥優惠票價為120元");
+                        System.out.println("please type your count number:");
                         int choicecount = scanner.nextInt();
                         aladdin.count = choicecount;
                         total = aladdin.count * (aladdin.price-30);
                         System.out.println(aladdin.menu + " " + "您購買的數量:" + aladdin.count + " " + "總額:" + total);
                         onmoive.moive();
                     } else if (choice == 2) {
-                        System.out.println("please type your count");
+                        System.out.println("現在時間是" + Hours + "所以是早鳥優惠票價為120元");
+                        System.out.println("please type your count number:");
                         int choicecount = scanner.nextInt();
                         dumbo.count = choicecount;
                         total = dumbo.count * (dumbo.price-30);
                         System.out.println(dumbo.menu + " " + "您購買的數量:" + dumbo.count + " " + "總額:" + total);
                         onmoive.moive();
                     } else if (choice == 3) {
-                        System.out.println("please type your count");
+                        System.out.println("現在時間是" + Hours + "所以是早鳥優惠票價為120元");
+                        System.out.println("please type your count number:");
                         int choicecount = scanner.nextInt();
                         mermaid.count = choicecount;
                         total = mermaid.count * (mermaid.price-30);
@@ -58,23 +60,25 @@ public class Tester {
                 }
             }
             else if (Hours < 12 || Hours <= 24) {
-                System.out.println("現在時間是" + Hours + "所以是晚上一般票為150元");
                 if (choice == 1) {
-                    System.out.println("please type your count");
+                    System.out.println("現在時間是" + Hours + "所以是晚上一般票為150元");
+                    System.out.println("please type your count number:");
                     int choicecount = scanner.nextInt();
                     aladdin.count = choicecount;
                     total = aladdin.count * aladdin.price;
                     System.out.println(aladdin.menu + " " + "您購買的數量:" + aladdin.count + " " + "總額:" + total);
                     onmoive.moive();
                 } else if (choice == 2) {
-                    System.out.println("please type your count");
+                    System.out.println("現在時間是" + Hours + "所以是晚上一般票為150元");
+                    System.out.println("please type your count number:");
                     int choicecount = scanner.nextInt();
                     dumbo.count = choicecount;
                     total = dumbo.count * dumbo.price;
                     System.out.println(dumbo.menu + " " + "您購買的數量:" + dumbo.count + " " + "總額:" + total);
                     onmoive.moive();
                 } else if (choice == 3) {
-                    System.out.println("please type your count");
+                    System.out.println("現在時間是" + Hours + "所以是晚上一般票為150元");
+                    System.out.println("please type your count number:");
                     int choicecount = scanner.nextInt();
                     mermaid.count = choicecount;
                     total = mermaid.count * mermaid.price;
